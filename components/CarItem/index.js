fa6d19a63922d18cb8a3a5d837f0e720323f6e0f
Nicolas Diot <nicolasdiot10@gmail.com>
2021-04-07 14:55:51 +0200

    TeslaClone done on homepage

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -5,7 +5,7 @@ import Button from '../Button';
 
 const CarItem = (props) => {
 
-  const { name, tagline, taglineCTA, image } = props;
+  const { name, tagline, taglineCTA, image } = props.car;
 
   return (
     <View style={styles.carContainer}>
